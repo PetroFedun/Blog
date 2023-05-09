4eed8c9b7cfe6258debbfdf3c6b5d625c7718b24
PetroFedun <fedynpetro@gmail.com>
2023-05-09 18:47:17 +0300

    Update comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   include Visible
 
-  validates :body, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 1 }
   
   belongs_to :article
   belongs_to :author, class_name: 'User', foreign_key: :author_id
